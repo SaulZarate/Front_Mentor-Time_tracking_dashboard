f9066ef3736d510ff37e7ea5d0951b722ad74463
saul zarate <saul.zarate@davinci.edu.ar>
2022-01-12 18:12:07 -0300

    Optimización de javascript

--- assets/index.js
+++ assets/index.js
@@ -1,80 +1,66 @@
 let data = []
+
+/* // Para pruebas en local
+fetch('../data.json') */
+/* Deployment in Github */
 fetch('https://github.com/SaulZarate/Front_Mentor-Time_tracking_dashboard/blob/main/data.json')
     .then(res => res.json())
     .then(info => {
         data = info
-        console.log(data)
     })
-    .catch(err => console.log(err))
+    .catch(err => console.log("No se encontraron los datos", err))
 
 
 window.addEventListener('DOMContentLoaded', () => {
 
     /* Navbar */
-    const li_Daily = document.getElementById('nav_item_Daily')
-    const li_Weekly = document.getElementById('nav_item_Weekly')
-    const li_Monthly = document.getElementById('nav_item_Monthly')
+    const content_nav_items = document.getElementById('content_nav_items')
+    content_nav_items.addEventListener('click', handleNavbar)
 
+    /* Tag to update */
     const title_jobs = document.getElementsByClassName('jobs_title')
     const hora_actual = document.getElementsByClassName('hora-actual')
     const hora_anterior = document.getElementsByClassName('hora-anterior')
 
-    li_Daily.addEventListener('click', clickDay)
-    li_Weekly.addEventListener('click', clickWeek)
-    li_Monthly.addEventListener('click', clickMonth)
 
-    /* Work */
+    function handleNavbar(e){
+        switch (e.target.textContent) {
+            case "Daily":
+                filterData("Daily")
+                break;
 
-    function clickDay() {
-        console.log('click Day')
-        const info_days = data.reduce((a, b) => ([
-            ...a, {
-                title: b.title,
-                time: {
-                    current: `${b.timeframes.daily.current}hrs`,
-                    previous: `Last Day - ${b.timeframes.daily.previous}hrs`,
-                }
-            }
-        ]), [])
-        updateDOM(info_days)
+            case "Weekly":
+                filterData("Weekly")
+                break;
+
+            case "Monthly":
+                filterData("Monthly")
+                break;
+
+            default:
+                // ....
+                break;
+        }
     }
-    function clickWeek() {
-        console.log('click Week')
-        const info_week = data.reduce((a, b) => ([
+
+    function filterData(dateType){
+        const info_date = data.reduce((a, b) => ([
             ...a, {
                 title: b.title,
                 time: {
-                    current: `${b.timeframes.weekly.current}hrs`,
-                    previous: `Last Week - ${b.timeframes.weekly.previous}hrs`,
+                    current: `${b.timeframes[dateType.toLowerCase()].current}hrs`,
+                    previous: `Last ${dateType} - ${b.timeframes[dateType.toLowerCase()].previous}hrs`,
                 }
             }
         ]), [])
-        updateDOM(info_week)
-    }
-    function clickMonth() {
-        console.log('click Month')
-        const info_month = data.reduce((a, b) => ([
-            ...a, {
-                title: b.title,
-                time: {
-                    current: `${b.timeframes.monthly.current}hrs`,
-                    previous: `Last Month - ${b.timeframes.monthly.previous}hrs`,
-                }
-            }
-        ]), [])
-        updateDOM(info_month)
+        updateDOM(info_date)
     }
 
     function updateDOM(newInfo) {
-        console.log(newInfo)
-
         newInfo.forEach((el, index) => {
-
             title_jobs[index].textContent = el.title
             hora_actual[index].textContent = el.time.current
             hora_anterior[index].textContent = el.time.previous
-
-
-        });
+        })
     }
 })
